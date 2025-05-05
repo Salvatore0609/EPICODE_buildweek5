c6cd4ade304528e8450e4af299edbe37660f0249
Salvatore0609 <salvatoredesole1998@gmail.com>
2025-05-05 10:44:07 +0200

    Initial commit

---
+++ src/main/java/it/epicode/EPICODE_buildweek5/EpicodeBuildweek5Application.java
@@ -0,0 +1,13 @@
+package it.epicode.EPICODE_buildweek5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EpicodeBuildweek5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EpicodeBuildweek5Application.class, args);
+	}
+
+}
---
+++ src/test/java/it/epicode/EPICODE_buildweek5/EpicodeBuildweek5ApplicationTests.java
@@ -0,0 +1,13 @@
+package it.epicode.EPICODE_buildweek5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EpicodeBuildweek5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
